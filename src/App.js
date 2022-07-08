fc94580cb3984ddcd27620aaffab970557d417c6
Isuru Atthanayake <79752561+Isuru2Atthanayake@users.noreply.github.com>
2022-07-08 13:36:21 +0530

    likes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 
 function App() {
-  const title ='Welcome to the new Blog';
-
+  const title ='Welcome to the new Blog';//constant variable
+  const likes = 50; //constant variable
   return (
     <div className="App">
       <div className="content">
         {/* <h1>App Component</h1>  */}
-        <h1>{title}</h1>
-        
+        <h1>{title}</h1> {/*the above constant variable is called here*/}
+        <p>Liked {likes} times</p>
 
       </div>
     </div>
