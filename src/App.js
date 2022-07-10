66aead506595a99667ddcf22f83b3711c758b1cb
Isuru Atthanayake <79752561+Isuru2Atthanayake@users.noreply.github.com>
2022-07-10 21:45:09 +0530

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
-import './App.css';
-
+//import './App.css';
+import Navbar from './Navbar'; //*importing Navbar from Navbar.js
+import Home from './Home';
+//"npm run start" -to run the project
 function App() {
   const title ='Welcome to the new Blog';//constant variable
   const likes = 50; //constant variable
@@ -9,17 +10,20 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar/>  {/*self closed navbar */}
       <div className="content">
+    
         {/* <h1>App Component</h1>  */}
-        <h1>{title}</h1> {/*the above constant variable is called here*/}
-        <p>Liked {likes} times</p>
+        {/* <h1>{title}</h1> the above constant variable is called here */}
+        {/* <p>Liked {likes} times</p> */}
         {/* <p>{person}</p> objects cannot be called*/}
-        <p>10</p>
-        <p>"hello,isuru"</p>
-        <p>{[1,2,3,4,5,6,7,8,9]}</p>{/* this is an array*/}
-        <p>{Math.random()*10}</p>
+        {/* <p>10</p> */}
+        {/* <p>"hello,isuru"</p> */}
+        {/* <p>{[1,2,3,4,5,6,7,8,9]}</p>this is an array */}
+        {/* <p>{Math.random()*10}</p> */}
 
-        <a href={link}>Youtube</a>
+        {/* <a href={link}>Youtube</a> */}
+        <Home/>
       </div>
     </div>
   );
