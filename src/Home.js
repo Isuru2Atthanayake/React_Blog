32bf4501281e081a2d585dd8d2feb7e19903ed21
Isuru Atthanayake <79752561+Isuru2Atthanayake@users.noreply.github.com>
2022-07-12 12:18:08 +0530

    css styles

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from "react"; //use state hook
 
 const Home = () => {
-    const[blogs, setBlogs] = useState([
+    const[blogs, setBlogs] = useState([  //list stored inside a use state
         {title: 'My new website',body:'lorem ipsum....',author:'mario', id:1},
         {title: 'WELCOME PARTY',body:'lorem ipsum....',author:'yoshi', id:2},
         {title: 'Web dev top tips',body:'lorem ipsum....',author:'sam', id:3}
@@ -9,10 +9,11 @@ const Home = () => {
 
     return ( 
     <div className="Home">
-        {blogs.map((blog)=>(
-            <div className="blog-preview" key={blog.id}>
+        {blogs.map((blog)=>( // map used to go through each item 
+            <div className="blog-preview" key={blog.id}> {/* unique key property*/}
                 <h2>{blog.title}</h2>  {/*displays the blog tittle */}
                 <p>Written by {blog.author}</p>
+                {/* <p>{blog.body}</p> */}
             </div>
         ))}
        
