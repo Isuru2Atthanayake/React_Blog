4e5c5328777abfc51d9c4e4d88c0cbc07305d8ed
Isuru Atthanayake <79752561+Isuru2Atthanayake@users.noreply.github.com>
2022-07-12 11:47:38 +0530

    Map

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,22 @@
 import { useState } from "react"; //use state hook
 
 const Home = () => {
-    // let name = 'mario'
-//    const[name,setName] = useState('mario'); // array to store the values /userstate hook
-//    const[age, setAge] = useState(20);
-    
-
-//     const handleClick = () => {
-//         setName('luigi');
-//         setAge(30);
-//     }
-
-    // const handleClick = (e) => {
-    //     console.log('Hello Isuru' , e);
-    // }
-
-    // const handleClickAgain = (name, e) => {
-    //     console.log('Hello ' + name,e.target);
-    // }
-
-
-    // let name1= 'mario'; //to change the name when on clicked 
-    // const handleClickChangename = () => {
-    //     name1 = 'luigi';
-    //     console.log(name1)
-    // }
-
+    const[blogs, setBlogs] = useState([
+        {title: 'My new website',body:'lorem ipsum....',author:'mario', id:1},
+        {title: 'WELCOME PARTY',body:'lorem ipsum....',author:'yoshi', id:2},
+        {title: 'Web dev top tips',body:'lorem ipsum....',author:'sam', id:3}
+    ]);
 
     return ( 
     <div className="Home">
-        <h2>Homepage</h2>
-        {/* <p>{name1}</p> */}
-        {/* <p>{name} is {age} years old</p>
-        <button onClick={handleClick}>Click me</button>  */}
-        {/* <button onClick={(e) => handleClickAgain('Sam', e)}>Click me again</button>
-        <button onClick={handleClickChangename}>Click me ch</button> */} 
+        {blogs.map((blog)=>(
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>  {/*displays the blog tittle */}
+                <p>Written by {blog.author}</p>
+            </div>
+        ))}
+       
+  
     </div> 
     );
 }
